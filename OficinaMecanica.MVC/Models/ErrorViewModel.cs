81ea6f4d9463f3564c0a13861c5157aab83bd615
gutofm <gutofisco@gmail.com>
2025-10-07 20:06:43 -0300

    Oficina Mecanica MVC project

---
+++ OficinaMecanica.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OficinaMecanica.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
